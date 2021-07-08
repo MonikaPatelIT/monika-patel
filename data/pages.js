e1ea7304695fa85abba07b8403db2a72a1c7e660
Monika Patel <monika.patel@homecaremedical.co.nz>
2021-07-08 20:20:27 +1200

    share development version

--- data/pages.js
+++ data/pages.js
@@ -1,52 +1,59 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     id: "about-me",
-    content: `<h1>Hello</h1><p>I am Monika Patel.</p>`,
+    content: `<div class="details"><h3>Hello!</h3><h4>I am Monika.</h4>
+    <p>This site is currently under development. To know more about me please <a href="https://monikapatelit.github.io/">visit</a>. </p></dvi>`,
     order: 1,
   },
-  {
-    id: "work-1",
-    content: `<h1>ACE rental cars</h1><p>Front end developer</p>`,
-    order: 4,
-  },
-  {
-    id: "work-2",
-    content: `<h1>Solution dynamics</h1><p>.Net developer</p>`,
-    order: 3,
-  },
-  {
-    id: "work-3",
-    content: `<h1>Whakarongarau Aoterau</h1><p>React developer</p>`,
-    projects: [
-      {
-        id: "project-1",
-        caption: "Quitline - Me Mutu",
-        img: "static/projects/quit.png",
-        url: "",
-        order: 1,
-      },
-      {
-        id: "project-1",
-        caption: "Quitline - Me Mutu",
-        img: "static/projects/quit.png",
-        url: "",
-        order: 1,
-      },
-      {
-        id: "project-1",
-        caption: "Quitline - Me Mutu",
-        img: "static/projects/quit.png",
-        url: "",
-        order: 1,
-      },
-      {
-        id: "project-1",
-        caption: "Quitline - Me Mutu",
-        img: "static/projects/quit.png",
-        url: "",
-        order: 1,
-      },
-    ],
-    order: 2,
-  },
+  // {
+  //   id: "education",
+  //   content: `<h1>CHARUSAT</h1><p>Bachlor Degree</p>`,
+  //   order: 5,
+  // },
+  // {
+  //   id: "work-1",
+  //   content: `<h1>ACE rental cars</h1><p>Front end developer</p>`,
+  //   order: 4,
+  // },
+  // {
+  //   id: "work-2",
+  //   content: `<h1>Solution dynamics</h1><p>.Net developer</p>`,
+  //   order: 3,
+  // },
+  // {
+  //   id: "work-3",
+  //   content: `<h1>Whakarongarau Aoterau</h1><p>React developer</p>`,
+  //   projects: [
+  //     {
+  //       id: "project-1",
+  //       caption: "Quitline - Me Mutu",
+  //       img: "static/projects/quit.png",
+  //       url: "",
+  //       order: 1,
+  //     },
+  //     {
+  //       id: "project-1",
+  //       caption: "Quitline - Me Mutu",
+  //       img: "static/projects/quit.png",
+  //       url: "",
+  //       order: 1,
+  //     },
+  //     {
+  //       id: "project-1",
+  //       caption: "Quitline - Me Mutu",
+  //       img: "static/projects/quit.png",
+  //       url: "",
+  //       order: 1,
+  //     },
+  //     {
+  //       id: "project-1",
+  //       caption: "Quitline - Me Mutu",
+  //       img: "static/projects/quit.png",
+  //       url: "",
+  //       order: 1,
+  //     },
+  //   ],
+  //   order: 2,
+  // },
 ];
